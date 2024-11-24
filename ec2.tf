1fc1ee6b81d9754c06a5006f2e8cc67988955d10
Akusnadi <Akusnadi@tokiomarine-life.co.id>
2024-11-24 15:06:53 +0700

    ok

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+variable "ami" {
+  type    = string
+  default = "ami-081205ca71b3f3635"
+}
+
+variable "infra_env" {
+  default = "ec2-machine"
+}
+
+variable "instace_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "keyname" {
+  type    = string
+  default = "keypair1"
+}
+
+variable "azs" {
+  type    = string
+  default = ap-southeast-3
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0106501aa5326af88"
+}
+
+
+variable "instance_name" {
+  default = "monitoring"}
