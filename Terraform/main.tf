ff73984208203e736ff4495136f4bf8615796ab3
Akusnadi <Akusnadi@tokiomarine-life.co.id>
2024-11-25 22:38:53 +0700

    ok

---
+++ Terraform/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "dev_machine" {
+  ami = data.aws_ami.amazon-linux.id
+  instance_type = "t3.micro"
+  key_name = "keypair123"
+
+  tags = {
+    Environment = "dev"
+    Name = "${var.name}-server"
+  }
+}
---
+++ Terraform/providers.tf
@@ -0,0 +1,3 @@
+provider "aws" {
+  region = "ap-southeasr-3"
+}
