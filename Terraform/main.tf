23f431f456b38a238ade6fe1c74e2fb2448f3ddb
Akusnadi <Akusnadi@tokiomarine-life.co.id>
2024-11-25 23:27:40 +0700

    ami-update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "amazon-linux" {
 
   filter {
     name   = "name"
-    values = ["ami-081205ca71b3f3635"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240927"]
   }
 
   filter {
@@ -13,7 +13,7 @@ data "aws_ami" "amazon-linux" {
 }
 
 resource "aws_instance" "dev_machine" {
-  ami = ami-081205ca71b3f3635
+  ami = data.aws_ami.amazon-linux.id
   instance_type = "t3.micro"
   key_name = "keypair123"
 
